dd0f7f3f3390fb07183fd26f97b7052fa2813c56
WalterNavarrete <francisconava125@gmail.com>
2017-09-01 02:49:40 -0300

    Prueba

---
+++ EjercicioDeClase4/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador suma1 = new Sumador();
+            Sumador suma2 = new Sumador();
+
+            Console.WriteLine("SUMA UNO");
+
+            Console.WriteLine(suma1.Sumar(4, 6));
+            Console.WriteLine(suma1.Sumar(5, 5));
+            Console.WriteLine(suma1.Sumar(3, 7));
+
+            Console.WriteLine("SUMA DOS");
+
+            Console.WriteLine(suma2.Sumar(10, 5));
+            Console.WriteLine(suma2.Sumar(8, 7));
+            Console.WriteLine(suma2.Sumar("hola", "chau"));
+
+            
+            
+
+            if (suma1 | suma2)
+            {
+                Console.WriteLine("Tiene igual cantidad de sumas");
+            }
+            else
+            {
+                Console.WriteLine("La cantidad de sumas son distintas");
+            }
+
+            long sumas = suma1 + suma2;
+
+            Console.WriteLine("La cantidad de suma tiene es : {0}", sumas);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjercicioDeClase4/ConsoleApplication1/Sumador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Sumador
+    {
+        private int cantidadSumas;
+        
+        
+        public Sumador() 
+        {
+            this.cantidadSumas = 0;
+        }
+        public Sumador(int cantSumas) 
+        {
+            this.cantidadSumas = cantSumas;
+        }
+
+         public long Sumar(long a, long b) 
+        {
+            cantidadSumas++;
+            return a + b;
+        }
+         public string Sumar(string a, string b)
+         {
+             cantidadSumas++;
+             return a + b;
+         }
+
+         
+         public static explicit operator long(Sumador sum) 
+         {
+             return sum.cantidadSumas;
+         }
+         public static long operator +(Sumador a, Sumador b) 
+         {
+             return a.cantidadSumas + b.cantidadSumas;
+         }
+
+         public static bool operator |(Sumador a, Sumador b) 
+         {
+             return (a.cantidadSumas == b.cantidadSumas);
+         }
+
+
+         public static bool operator ==(Sumador a, Sumador b) 
+         {
+             return a.cantidadSumas == b.cantidadSumas;
+         }
+        public static bool operator !=(Sumador a, Sumador b)
+        {
+            return !(a == b);
+        }
+    }
+}
